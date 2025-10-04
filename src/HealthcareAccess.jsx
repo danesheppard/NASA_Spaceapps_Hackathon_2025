92f130881dbba72cc707b8a863413bd6fcbbe4e9
Dane Sheppard <danesheppard@gmail.com>
2025-10-04 20:20:53 -0300

    Pollution removed large files

--- src/HealthcareAccess.jsx
+++ src/HealthcareAccess.jsx
@@ -6,14 +6,35 @@ import "./App.css";
 import { NavLink } from "react-router";
 import { DeckGL } from "@deck.gl/react";
 import { TileLayer } from "@deck.gl/geo-layers";
-import { BitmapLayer } from "@deck.gl/layers";
+import { BitmapLayer, GeoJsonLayer, PolygonLayer } from "@deck.gl/layers";
+import { RasterTileLayer, rasterSource } from "@deck.gl/carto";
 import { ZoomWidget } from "@deck.gl/react";
+import { scaleThreshold } from "d3-scale";
 
 const INITIAL_VIEW_STATE = {
-  longitude: -122.41669,
-  latitude: 37.7853,
+  longitude: -84.217296,
+  latitude: 33.932465,
   zoom: 13,
 };
+
+// Scale colour from green (low pollution) to red (high pollution)
+const COLOR_SCALE = scaleThreshold()
+  .domain([0, 0.2, 0.4, 0.6, 0.8, 1.0])
+  .range([
+    [0, 255, 0],
+    [127, 255, 0],
+    [255, 255, 0],
+    [255, 127, 0],
+    [255, 0, 0],
+  ]);
+
+const POLLUTION_DATA_URL =
+  "../pollutionExposure/processedData/ec_2019_georgia.geojson";
+
+// const rasterData = new rasterSource({
+//   accessToken: "local",
+//   url: "/layerData/GHS_BUILT_S_E2025_GLOBE_R2023A_54009_100_V1_0_R5_C11.tif",
+// });
 
 const layers = [
   new TileLayer({
@@ -30,6 +51,28 @@ const layers = [
         bounds: [west, south, east, north],
       });
     },
+  }),
+  // new PolygonLayer({
+  //   id: "Pollution grid",
+  //   data: POLLUTION_DATA_URL,
+  //   filled: true,
+  //   getPolygon: (d) => d.geometry.coordinates,
+  // }),
+  new GeoJsonLayer({
+    id: "Pollution data",
+    data: POLLUTION_DATA_URL,
+    filled: true,
+    getFillColor: (d) => {
+      const ecValue = d.properties.ec_normalized;
+      return COLOR_SCALE(ecValue);
+    },
+    getElevation: (f) => {
+      // We want the elevation to scale fairly dramatically to make the differences more visible
+      const ecValue = f.properties.ec * 100;
+      return Math.max(0, ecValue * ecValue); // Scale factor of 500
+    },
+    extruded: true,
+    pickable: true,
   }),
 ];
 
